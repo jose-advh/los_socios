4e92dae3628b8fac25f633e0388a74376bc90379
Sala de Sistemas 1-6 / Sede 1 <sala161@CIACADEMICO.LOCAL>
2024-09-30 11:13:06 -0500

    Algunos cambios

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -1,6 +1,6 @@
 <?php
 
-$conexion = new mysqli("localhost", "root", "", "joyeria_socios");
+$conexion = new mysqli("localhost", "root", "", "prueba123");
 //Para que el programa pueda comprender los caracteres especiales.
 $conexion->set_charset("utf8");
 
